9d249057128d328ec7e51e6b16c24e86fbbf904a
P41T0 <tortmasoliver@gmail.com>
2023-12-17 18:24:27 +0100

    missatge

---
+++ app/src/main/java/com/uvic/ad32023/pTort/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.uvic.ad32023.pTort;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projectefinal.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        final long DELAY = 2000; //5000 milisegons = 5 segons
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, PantallaPrincipal.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY);
+    }
+}
