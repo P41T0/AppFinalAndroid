191dd523655eedad08708a14078e9a1dfbb71cda
P41T0 <tortmasoliver@gmail.com>
2023-12-19 20:32:08 +0100

    Per fi funciona aquesta merda de guardar imatges de la galeria

--- app/src/main/java/com/uvic/ad32023/pTort/PauTort.java
+++ app/src/main/java/com/uvic/ad32023/pTort/PauTort.java
@@ -3,6 +3,7 @@ package com.uvic.ad32023.pTort;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class PauTort extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class PauTort extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pau_tort);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
